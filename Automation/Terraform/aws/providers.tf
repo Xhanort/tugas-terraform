b341156ec1062712754d24258bf2b603dd46040a
Xhanort <faldo.satria@gmail.com>
2026-01-06 09:36:04 +0700

    providers.tf

--- Automation/Terraform/aws/providers.tf
+++ Automation/Terraform/aws/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-southeast-3"
-  access_key = "AKIAWH34WE3D2Y5GCT22"
-  secret_key = "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"
+  access_key = 
+  secret_key = 
 }
